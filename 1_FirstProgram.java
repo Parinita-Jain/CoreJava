866f91d7976556136845736407eaa6855628eb9c
Parinita-Jain <69913597+Parinita-Jain@users.noreply.github.com>
2024-03-04 21:40:30 +0530

    Add files via upload

---
+++ 1_FirstProgram.java
@@ -0,0 +1,10 @@
+class Program
+{
+	public static void main(String[] args){
+		System.out.println("welcome to the world of java");
+	}
+}
+
+class Greeting{
+	
+}
---
+++ 1_Variables.java
@@ -0,0 +1,22 @@
+class Variables{
+	public static void main(String[] args){
+		//Whole Numbers
+		byte b = 127;
+		short s = 128;
+		int i = 8444854;
+		long ll = 847678456781L;
+
+		//Decimal Numbers
+		float f = 674.45f;
+		double d = 4657.434;
+
+		//Character
+		char c = 'a';
+		char cc = 89;
+
+		//Boolean
+		boolean bb = true;
+
+		System.out.println(cc);
+	}
+}
